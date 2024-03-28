1fc5696b6a59a27b7830d15c0018feabb97dad25
Pranai-1 <reddypranai2017@gmail.com>
2024-03-28 11:35:03 +0530

    text

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -10,9 +10,9 @@ export default function Signup() {
   return (
     <div className=" h-full w-full bg-black absolute flex justify-center items-center">
       <div className="bg-white  w-[360px] md:w-[440px] flex flex-col gap-2 mb-5 p-5 rounded-xl justify-center">
-        <h1 className="font-bold text-center text-2xl text-orange-600">
+        <p className="font-bold text-center text-2xl text-orange-600">
           Signup to BuzzBox
-        </h1>
+        </p>
         <p className="font-medium text-center text-gray-600">
           Start your journey
         </p>
